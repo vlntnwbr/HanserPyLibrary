b0612951175009638c275307705bbe8b6e597854
Valentin Weber <vweber@stud.hs-heilbronn.de>
2020-02-08 02:57:36 +0100

    v0.1.4 Added cmd testfile
    * Includes ISBN-10, ISBN-13 and DOI URLs
    * Includes --isbn
    * Includes no-access book
    * Includes force creating custom directory

---
+++ test/test_isbn_doi_dir.cmd
@@ -0,0 +1,6 @@
+py -m hanser_py_library.main ^
+  https://hanser-elibrary.com/isbn/9783446446991 ^
+  https://www.hanser-elibrary.com/doi/book/10.3139/9783446424401 ^
+  www.hanser-elibrary.com/isbn/9783446423824 ^
+  hanser-elibrary.com/isbn/9783446440685 ^
+  -o ./downloads -f --isbn 9783446446991
